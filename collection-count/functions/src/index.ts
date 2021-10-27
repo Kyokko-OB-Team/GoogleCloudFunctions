593868bc3ad78c5beb079eefa1c67606c2a28d12
yoshiki <gariyoshi0630@gmail.com>
2021-10-28 08:01:43 +0900

    firestore上の最新データを取得するFunctionの作成

--- collection-count/functions/src/index.ts
+++ collection-count/functions/src/index.ts
@@ -16,15 +16,26 @@ const fireStore = admin.firestore();
 
 // collectionsのdocuments数カウント
 exports.countCollections = functions.https.onRequest((req, res) => {
-  const collectionName = req.body["collection"];
+  let collectionName = "";
+  let eolCode = "";
+  if (req.method === "POST") {
+    collectionName = req.body["collection"];
+    eolCode = "\n";
+  }
+  if (req.method === "GET") {
+    if (req.query.collection !== undefined) {
+      collectionName = req.query.collection.toString();
+      eolCode = "<br>";
+    }
+  }
   const collectionsRef = fireStore.collection(collectionName);
   collectionsRef.get().then((snap) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-HEaders",
         "Origin, X-Requested-With, Content-Type, Accept");
-    let strOut = "\n";
-    strOut += collectionName + "\n";
-    strOut += "lenght: " + snap.size + "\n";
+    let strOut = eolCode;
+    strOut += collectionName + eolCode;
+    strOut += "lenght: " + snap.size + eolCode;
     res.status(200).send(strOut);
   })
       .catch((error) => {
@@ -33,35 +44,40 @@ exports.countCollections = functions.https.onRequest((req, res) => {
 });
 
 // Documentの一番新しいデータを取得
-exports.getNewDocument = functions.https.onRequest((req, res) => {
+exports.getLatestDocument = functions.https.onRequest(async (req, res) => {
 //  const collenctionName = req.body["collection"];
-  const collectionRef = fireStore.collection("sensor-data_test-env");
-  collectionRef.orderBy("time").limit(1).get().then((snap) => {
-    const temp = snap.doc().get("temperature");
-    const humi = snap.doc().get("humidity");
-    const co2 = snap.doc().get("co2-concentration");
-    const unixtime = snap.doc().get("time");
+  let eolCode = "";
+  if (req.method === "POST") {
+    eolCode = "\n";
+  }
+  if (req.method === "GET") {
+    eolCode = "<br>";
+  }
 
-    // 時間変換
-    let date = new Date(unixtime * 1000);
+  const db = admin.firestore();
+  const collectionRef = db.collection("sensor-data_test-env");
+  const snapshot = await collectionRef.orderBy("time", "desc").limit(1).get();
+  const temp = snapshot.docs[0].get("temperature");
+  const humi = snapshot.docs[0].get("humidity");
+  const co2 = snapshot.docs[0].get("co2-concentration");
+  const unixtime = snapshot.docs[0].get("time");
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    let strOut = "\n";
-    strOut += "sensor-data_test-env\n";
-    strOut += "最新データ\n";
-    strOut += "時間:";
-    strOut += date.toLocaleDateString("ja-JP");
-    strOut += date.toLocaleTimeString("ja-JP") + "\n";
-    strOut += "温度:" + "\n";
-    strOut += "湿度:" + "\n";
-    strOut += "CO2濃度:" + "\n";
-    res.status(200).send(strOut);
-  })
-      .catch((error) => {
-        res.send(error());
-      });
+  // 時間変換
+  const date = new Date(unixtime * 1000);
+  date.setHours(date.getHours() + 9);
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+  let strOut = eolCode;
+  strOut += "sensor-data_test-env" + eolCode;
+  strOut += "最新データ" + eolCode;
+  strOut += "時間: ";
+  strOut += date.toLocaleString("ja-JP") + eolCode;
+  strOut += "温度: " + temp.toFixed(2) + eolCode;
+  strOut += "湿度: " + humi.toFixed(2) + eolCode;
+  strOut += "CO2濃度: " + co2 + eolCode;
+  res.status(200).send(strOut);
 });
 
 // Batch フィールドに時間の情報を追加
@@ -70,8 +86,7 @@ exports.setTimeData = functions.https.onRequest(async (req, res) => {
 
   const db = admin.firestore();
   let batch = db.batch();
-  const snapshots = await db.collection(collectionName)
-      .get();
+  const snapshots = await db.collection(collectionName).get();
   let log = "";
   res.send("function exec.\n");
 
